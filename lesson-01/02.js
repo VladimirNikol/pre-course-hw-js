1760deec38ed99fc3b68d92faf176cdc57f4de3d
Vladimir <vovchik.niko@gmail.com>
2024-01-04 14:50:38 +0300

    test-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,8 @@
-// code
+let num = Math.floor(Math.random() * 100);
+let resultStr;
+if(num % 2 === 0){
+    resultStr = ':Это четное число'
+} else {
+    resultStr = ':Это нечетное число'
+}
+console.log(num, resultStr)
