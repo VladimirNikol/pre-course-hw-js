e17294b9c913a90a66dfe075cc02239daa31f677
Vladimir <vovchik.niko@gmail.com>
2024-01-16 12:20:34 +0300

    test-1/3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter( (n) => {
+        if (n === "черный" || n === "красный" || n === "желтый"){
+            return n
+        }
+    } ).join('-')
+}
+
+console.log(createColorString());
