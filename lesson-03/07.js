90f142e5ec3c65452044d38080ac39a75108b4a8
Vladimir <vovchik.niko@gmail.com>
2024-01-16 20:08:52 +0300

    test-7/3

---
+++ lesson-03/07.js
@@ -0,0 +1,16 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+let newArr = numbers.map((arr) => {
+    const arr2=[];
+    arr.map((num) => {
+            if (num % 2 === 0) {
+                arr2.push(num)
+            }
+        }
+    )
+    return arr2
+})
+console.log(newArr)
